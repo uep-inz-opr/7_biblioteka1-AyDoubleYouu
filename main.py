57c62563639ca3708236367fb8483a6f8f44f1e5
Adrian <league711@wp.pl>
2022-05-23 20:09:39 +0200

    fit

---
+++ main.py
@@ -0,0 +1,31 @@
+import ast as ast
+
+class Biblioteka():
+    
+    def __init__(self,tytul,autor,rok): 
+        self.tytul = tytul
+        self.autor = autor
+        self.rok = rok
+        self.ewidencja = 1
+        self.ksiazka = self.tytul,self.autor
+        
+    def __repr__(self):
+        return "'"+self.autor +""+ "'," + str(self.ewidencja)
+    
+
+ksiazka_dane = {}
+ilosc_ksiazek = int(input())
+for ilosc in range(ilosc_ksiazek):
+    ksiazka_wejsciowa = eval(input())
+    tytul = ksiazka_wejsciowa[0]
+    autor = ksiazka_wejsciowa[1]
+    rok = ksiazka_wejsciowa[2]
+    if tytul in ksiazka_dane:
+        ksiazka_dane[tytul].ewidencja +=1
+    else:
+        ksiazka_dane[tytul] = Biblioteka(tytul,autor,rok)
+
+posortowane_ksiazki_dane = sorted(ksiazka_dane.items())
+
+for ksiazka in posortowane_ksiazki_dane:
+    print(ksiazka)
